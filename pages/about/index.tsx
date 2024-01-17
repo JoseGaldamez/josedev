b529d9ec0e28ee3a42b47842c19428157dee0bd4
Jose Galdamez <josegaldamez1991@gmail.com>
2024-01-16 18:41:23 -0600

    adding meta tags

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -10,6 +10,13 @@ export const AboutPage = () => {
         <>
             <Head>
                 <title>Jose Galdamez | Acerca de mi</title>
+                <meta name="description"
+                    content="Desarrollador de aplicaciones web y móviles"
+                    key="desc" />
+                <meta
+                    property="og:image"
+                    content="https://www.josegaldamez.online/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjose-galdamez.af98a70d.png&w=384&q=75"
+                />
             </Head>
             <Navbar />
             <main>
