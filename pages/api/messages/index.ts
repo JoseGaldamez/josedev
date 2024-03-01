5477375c876795dd7938ae1a6dbe9e00d6f51a9d
Jose Galdamez <josegaldamez1991@gmail.com>
2024-03-01 09:36:05 -0600

    secure pass email

--- pages/api/messages/index.ts
+++ pages/api/messages/index.ts
@@ -1,16 +1,5 @@
-import nodemailer from "nodemailer";
 import { sendMessage } from "@/services/sendMessage";
 import { NextApiRequest, NextApiResponse } from "next";
-
-const transporter = nodemailer.createTransport({
-    host: "mail.codigocorrecto.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-        user: "josegaldamez@codigocorrecto.com", // your domain email address
-        pass: "lRylgm0B9Ei=", // your password
-    },
-});
 
 export default async function handler(
     request: NextApiRequest,
@@ -31,39 +20,7 @@ export default async function handler(
     }
 
     try {
-        const messageOptions = {
-            from: '"JoseGaldamezDev Website" <josegaldamez@codigocorrecto.com>',
-            to: "josegaldamez1991@gmail.com",
-            subject: "Message from JoseGaldamezDev Website.",
-            text: bodyMessage.message,
-        };
-
-        await transporter.sendMail(
-            messageOptions,
-            function (error: any, info: any) {
-                if (error) {
-                    return response.status(500).json({
-                        error: true,
-                        description: "Error sending email.",
-                        result: error,
-                    });
-                } else {
-                    console.log("Email sent: " + info.response);
-                    return response.status(201).json({
-                        error: false,
-                        description: "Message sent.",
-                        result: info.response,
-                    });
-                }
-            }
-        );
-
-        // const result = await sendMessage(bodyMessage.message);
-        // return response.status(201).json({
-        //     error: false,
-        //     description: "Message sent.",
-        //     result,
-        // });
+        await sendMessage(bodyMessage.message, response);
     } catch (error) {
         return response.status(500).json({ error: true, description: error });
     }
--- src/services/sendMessage.ts
+++ src/services/sendMessage.ts
@@ -1,32 +1,42 @@
-const nodemailer = require("nodemailer");
+import { NextApiResponse } from "next";
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "mail.codigocorrecto.com",
+    host: process.env.EMAIL_HOST,
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-        user: "josegaldamez@codigocorrecto.com", // your domain email address
-        pass: "lRylgm0B9Ei=", // your password
+        user: process.env.EMAIL_USERNAME, // your domain email address
+        pass: process.env.EMAIL_PASSWORD, // your password
     },
 });
 
-export const sendMessage = async (mesageBody: string) => {
+export const sendMessage = async (
+    mesageBody: string,
+    response: NextApiResponse
+) => {
     const messageOptions = {
         from: '"JoseGaldamezDev Website" <josegaldamez@codigocorrecto.com>',
         to: "josegaldamez1991@gmail.com",
         subject: "Message from JoseGaldamezDev Website.",
         text: mesageBody,
     };
-
-    return await transporter.sendMail(
+    await transporter.sendMail(
         messageOptions,
         function (error: any, info: any) {
             if (error) {
-                console.log(error);
-                return error;
+                return response.status(500).json({
+                    error: true,
+                    description: "Error sending email.",
+                    result: error,
+                });
             } else {
                 console.log("Email sent: " + info.response);
-                return info.response;
+                return response.status(201).json({
+                    error: false,
+                    description: "Message sent.",
+                    result: info.response,
+                });
             }
         }
     );
